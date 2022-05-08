cc0bc8fd0716eb894f78e9f64e27c7c378a2dba8
BaneyZ <770815705@qq.com>
2022-05-08 20:00:45 +0800

    MQ

---
+++ ProduceClient.java
@@ -0,0 +1,9 @@
+package MQ;
+
+
+public class ProduceClient {
+    public static void main(String[] args) throws Exception {
+        MQClient client = new MQClient();
+        client.produce("Hello World4!!");
+    }
+}
